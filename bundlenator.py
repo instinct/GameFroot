acf61e2fcd7e40e5f1a95541b0c9777cdac4f0a5
Sam Win-Mason <sam@albedosoftware.com>
2012-05-15 14:53:07 +1200

    Finished downloading asset code.

--- bundlenator.py
+++ bundlenator.py
@@ -2,30 +2,29 @@
 
 '''
 	1. 
-	create folder structure
-	Download list of games from website
+	- create folder structure
+	- Download list of games from website
 
 	2.
 
 	For each game in list:
-		* Check to see if not downloaded already
+		- * Check to see if not downloaded already
 		* If not then:
-			download json of level
-			download custom spritesheets
-			download custom backgrounds
-			download custom sfx
-			download custom music
-			download custom tilesheets
-			download custom weapon spritesheets
-			check for jump to level robot, if found for each level:
+			- download json of level
+			- download custom spritesheets
+			- download custom backgrounds
+			- download custom sfx
+			- download custom music
+			- download custom tilesheets
+			- download custom weapon spritesheets
+			- check for jump to level robot, if found for each level:
 				load level as above. (recurse)
 	
 	3. 	
 	write plist
-	4.
-	write consolidated games json
-	5.
-	write consolidated levels json 
+
+	* Add default bundle to check for existing files
+
 '''
 
 
@@ -76,15 +75,22 @@ import time
 import urllib
 import json
 
-# Some global config
+# Some global config strings, change as required
 
 GET_LEVELS_API_STRING = '?gamemakers_api=1&type=get_all_levels&category='
+GET_TILES_API_STRING = '?gamemakers_api=1&type=get_game_animations&id='
+GET_MAP_API_STRING = '?gamemakers_api=1&type=json&map_id='
+GET_BACKGROUND_URL = '/wp-content/plugins/game_data/backgrounds/user/'
+GET_CHARACTER_URL = '/wp-content/characters/'
+
+DEFAULT_BUNDLE_INFO = str('{ "product_id":0, "" }')
 
 
 def loadGameData():
 
 	global gamedata
-	print "retrieving game list for category " + args[0] + "..."
+
+	if options.verbose: print "retrieving game list for category " + args[0] + "..."
 
 	# get game data
 	requestString = args[1] + GET_LEVELS_API_STRING + args[0]
@@ -92,12 +98,13 @@ def loadGameData():
 	gamedata = json.load(gameDataFH)
 
 
+
 def loadAsset(dirName, urlString):
 	localFilePath = args[2] + "iap_bundle/" + dirName + "/" + os.path.split(urlString)[1]
 	if(not os.path.exists(localFilePath)):
 		try:
 			inetFH = urllib.urlopen(urlString)
-			print '[done]'
+			if options.verbose: print '[done]'
 			localFH = open(localFilePath, 'w')
 			localFH.write(inetFH.read())
 		except Exception, e:
@@ -105,25 +112,112 @@ def loadAsset(dirName, urlString):
 		else:
 			pass
 		finally:
-			print '[saved]'
+			if options.verbose: print '[saved]'
 
 
 
 def loadGameAssets():
+	global levelsDownloaded
+
 	print "retrieving game assets..."
+
+	levelsDownloaded = list()
 
 	# for each level, load the assets
 	for level in gamedata:
 		print "retrieving assets for '" + level['title'] + "'..."
 		# Thumb
-		print "		downloading thumb..."
-		loadAsset('thumbs',level['background'])
-		print "		downloading map data"
-		loadMapData(level['id'])
+		if options.verbose: print "		downloading thumb..."
+		# loadAsset('thumbs',level['background'])
+
+		if options.verbose: print "		downloading map data"
+		if level['id'] not in levelsDownloaded:
+			loadLevelAssets(level['id'])
+
+
 		
 
-def loadMapData():
-	pass	
+def loadLevelAssets(mapId):
+	localFilePath = args[2] + "iap_bundle/levels/" + `mapId` + ".map"
+	if(not os.path.exists(localFilePath)):
+		try:
+			inetFH = urllib.urlopen(args[1] + GET_MAP_API_STRING + `mapId`)
+			levelData = json.load(inetFH)
+			# localFH = open(localFilePath, 'w')
+			# localFH.write(inetFH.read())
+
+			# Load custom bg music
+			for music in levelData['sprites']['background_music']:
+				# loadAsset('music', music['url'])
+				pass
+
+			# Load backgrounds
+			# loadAsset('backgrounds', (args[1] + GET_BACKGROUND_URL + levelData['meta']['background']['image']))
+
+			# Load custom spritesheets
+			# Load enemies
+
+			sheetName = ''
+
+			for enemy in levelData['map']['characters']:
+				enemyNum = int(enemy['num'])
+				if(enemyNum < 100):
+					sheetName = 'enemy_sheet' + `enemyNum` + '.png'
+				else:
+					sheetName = 'enemy' + `enemyNum` + '.png'
+		
+			# print args[1] + GET_CHARACTER_URL + sheetName
+			# loadAsset('characters', args[1] + GET_CHARACTER_URL + sheetName)	
+
+			# Load character
+			if (int(levelData['map']['player']['num']) < 100):
+				sheetName = "player_" + `levelData['map']['player']['num']` + ".png"
+			else:
+				sheetName = "character" + `levelData['map']['player']['num']` + ".png"
+
+			# print args[1] + GET_CHARACTER_URL + sheetName
+			# loadAsset('characters', args[1] + GET_CHARACTER_URL + sheetName)
+
+			# Load custom SFX
+			for sfx in levelData['sprites']['sounds']:
+				# loadAsset('sfx', sfx['url']);
+				pass
+
+			# Load custom weapon spritesheets
+			for weapon in levelData['sprites']['weapons']:
+				# loadAsset('weapons', weapon['file'])
+				pass
+
+			# Load custom tilesets
+			# print args[1] + GET_TILES_API_STRING + `mapId`
+			# loadAsset('tiles', args[1] + GET_TILES_API_STRING + mapId)
+
+			levelsDownloaded.append(mapId)
+			connectedLevels = list()
+
+			# Get list of level jump robot scripts
+			for script in levelData['sprites']['robots']:
+				for behaviour in script['script']['behavior']:
+					for command in behaviour['commands']:
+						if command['action'] == 'gotoLevel':
+							connectedLevels.append(int(command['level']))
+			
+
+			# If not already downloaded, recursively load attached levels
+			for l in connectedLevels:
+				if l not in levelsDownloaded:
+					loadLevelAssets(int(l))
+
+		except Exception, e:
+			print "An error as occured downloading one or more assets [Failwhale]"
+			raise
+		else:
+			pass
+		finally:
+			pass
+
+			
+		
 
 
 def createDirectoryStructure():
@@ -141,6 +235,7 @@ def createDirectoryStructure():
 			os.mkdir("sfx")
 			os.mkdir("music")
 			os.mkdir("weapons")
+			os.mkdir("levels")
 
 		except Exception, e:
 			print "There was an error creating bundle directories at the specifed path [Failwhale]"
@@ -148,6 +243,9 @@ def createDirectoryStructure():
 	else :
 		print 'specified path does not exist [Failwhale]'
 
+
+def writePackageReceipt():
+	pass
 
 
 def main ():
@@ -161,9 +259,10 @@ def main ():
 	# Create folder structure
 	if(len(args) == 3):
 
-		createDirectoryStructure()
+		# createDirectoryStructure()
 		loadGameData()
 		loadGameAssets()
+		writePackageReceipt()
 
 	else:
 		print "USAGE: bundlenator [-h] [-v,--verbose] [--version] category_name endpoint_url bundle_output_path"
